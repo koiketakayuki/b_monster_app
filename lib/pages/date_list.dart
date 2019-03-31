cc4e85069b90b41e58910bbb4c90be3354b4e091
小池貴之 <takayuki.koike@bebit.com>
2019-03-31 23:25:59 +0900

    feat: add date list page

---
+++ lib/pages/date_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:b_monster_app/pages/app_shell.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+class DateList extends StatelessWidget {
+  static final String title = '日付を選択';
+  static final dateFormatter = DateFormat("M/dd(E)", "ja_JP");
+
+  @override
+  Widget build(BuildContext context) {
+    initializeDateFormatting("ja_JP");
+    List<DateTime> dates = getDateList();
+    Iterable<ListTile> tiles =
+        dates.map((date) => ListTile(title: Text(dateFormatter.format(date))));
+
+    return AppShell(
+        title,
+        ListView(
+            children:
+                ListTile.divideTiles(context: context, tiles: tiles).toList()),
+        hasDrawer: false
+    );
+  }
+
+  List<DateTime> getDateList() {
+    DateTime today = DateTime.now();
+    return [
+      today,
+      today.add(Duration(days: 1)),
+      today.add(Duration(days: 2)),
+      today.add(Duration(days: 3)),
+      today.add(Duration(days: 4)),
+      today.add(Duration(days: 5)),
+      today.add(Duration(days: 6)),
+    ];
+  }
+}
