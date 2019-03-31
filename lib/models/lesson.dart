2a9a1e3b5cbd418aedaff636a1e93cae241f7a8c
小池貴之 <takayuki.koike@bebit.com>
2019-03-31 23:00:12 +0900

    fix: fix Lesson model
    - override toString for debug
    - fix isReservable bug
    - add date property

--- lib/models/lesson.dart
+++ lib/models/lesson.dart
@@ -4,14 +4,20 @@ class Lesson {
   final String id;
   final Performer performer;
   final String programName;
+  final DateTime date;
   final String timeRangeLabel;
 
-  Lesson(this.id, this.performer, this.programName, this.timeRangeLabel);
+  Lesson(this.id, this.performer, this.programName, this.date, this.timeRangeLabel);
 
   @override
   operator ==(dynamic other) {
     return other is Lesson && id == other.id;
   }
+
+  bool isReservable() => programName != "STREAM ONLY" && programName != "無料体験会";
+
+  @override 
+  String toString() => "<Lesson($id): ${date.month}/${date.day} $timeRangeLabel $performer $programName>";
 
   int get hashCode => int.parse(id);
 }
